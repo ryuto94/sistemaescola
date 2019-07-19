1aed16373eb5b7e42c1285f629e6eaf4c0f279fc
Rodrigo Toguti <ryutoguti@hotmail.com>
2019-07-19 17:23:34 -0300

    Criando tabela com artisan

---
+++ database/migrations/2019_07_19_194041_criar_produtos.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CriarProdutos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function(Blueprint $table){
+            $table->bigIncrements('id');
+            $table->string('nome_produto');
+        });
+    }    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
